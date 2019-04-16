c9cf18d9c7112c466a7b4d225bd34fe276f74c44
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-04-16 17:25:38 -0500

    Config Vue Apollo and lint

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from "vue";
+import Vue from 'vue'
 import './plugins/vuetify'
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import { createProvider } from '@/plugins/vue-apollo'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
