b4d4d9f96162b488616bb862046ebb9bfb61e11a
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-05-01 12:07:23 -0500

    Adding AuthGuard and LazyLoading for Components

--- src/main.js
+++ src/main.js
@@ -8,26 +8,6 @@ import { apolloClient } from '@/plugins/vue-apollo'
 
 Vue.config.productionTip = false
 
-router.beforeEach((to, from, next) => {
-  if (
-    to.matched.some(record => record.meta.requiresAuth) &&
-    !store.getters['auth/isLoggedIn']
-  ) {
-    next({
-      name: 'home'
-    })
-  } else if (
-    to.matched.some(record => record.meta.requiresVisitor) &&
-    store.getters['auth/isLoggedIn']
-  ) {
-    next({
-      name: 'dashboard'
-    })
-  } else {
-    next()
-  }
-})
-
 new Vue({
   router,
   store,
--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import SignIn from '@/components/Auth/SignIn.vue'
-import SignOut from '@/components/Auth/SignOut.vue'
 
-import Dashboard from '@/views/Dashboard.vue'
+import store from '@/store'
+import { init } from '@/components/Auth/guards/AuthGuard'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -22,15 +22,29 @@ export default new Router({
     {
       path: '/logout',
       name: 'logout',
-      component: SignOut
+      // route level code-splitting
+      // this generates a separate chunk (signOut.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(
+          /* webpackChunkName: "signOut" */ '@/components/Auth/SignOut.vue'
+        )
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: Dashboard,
+      // route level code-splitting
+      // this generates a separate chunk (signOut.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "signOut" */ '@/views/Dashboard.vue'),
       meta: {
         requiresAuth: true
       }
     }
   ]
 })
+
+init(router, store)
+
+export default router
