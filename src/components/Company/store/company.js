94211b7cb72291b6903d6e8575873a3aa9f3292f
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-05-02 14:36:18 -0500

    Get company by id when page is reloaded

--- src/components/Company/store/company.js
+++ src/components/Company/store/company.js
@@ -1,7 +1,17 @@
+import companyService from '@/components/Company/services/CompanyService'
+
 const company = {
   namespaced: true,
   state: {
     company: null
+  },
+  getters: {
+    getCompanyName(state) {
+      return state.company ? state.company.company_legal_name : ''
+    },
+    getCompanyIdentification(state) {
+      return state.company ? state.company.company_identification : ''
+    }
   },
   mutations: {
     SET_COMPANY(state, payload) {
@@ -11,7 +21,11 @@ const company = {
       state.company = null
     }
   },
-  actions: {}
+  actions: {
+    retrieveData(context, payload) {
+      return companyService.retrieveData(context, payload)
+    }
+  }
 }
 
 export default company
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,11 +2,15 @@
   v-container
     h1.title.blue-dark--text.text--lighten-2 Welcome to the Jungle 
 </template>
-<<script>
+
+<script>
 export default {
   name: 'Dashboard',
   async created() {
     await this.$store.dispatch('user/retrieveData')
+    await this.$store.dispatch('company/retrieveData', {
+      userId: this.$store.state.user.user.user_id
+    })
   }
 }
 </script>
