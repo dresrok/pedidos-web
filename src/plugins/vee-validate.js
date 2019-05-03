3c07de9f679d594e8b7a0ff517147fa3c214074f
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-05-03 11:52:14 -0500

    Set vee-validate plugin and add custom rule

--- src/plugins/vee-validate.js
+++ src/plugins/vee-validate.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import VeeValidate, { Validator } from 'vee-validate'
 import es from 'vee-validate/dist/locale/es'
+import maxDimensionsRule from '@/plugins/maxDimensionsRule'
 
 Vue.use(VeeValidate)
 
 Validator.localize('es', es)
+
+Validator.extend('max_dimensions', maxDimensionsRule)
