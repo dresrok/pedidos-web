7fc4124d5f5a8532835be8e939d1f80b044046f7
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-05-03 21:56:27 -0500

    Setting spanish language in vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,16 +2,12 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 import '@fortawesome/fontawesome-free/css/all.css'
-import en from 'vuetify/es5/locale/en.js'
 import es from 'vuetify/es5/locale/es.js'
 
 Vue.use(Vuetify, {
   iconfont: 'fa',
   lang: {
-    locales: {
-      es,
-      en
-    },
+    locales: { es },
     current: 'es'
   }
 })
